981fa7419a36f7050f780d84e5fba4796cccc7dd
yukireonsousi <yukireonsousi@gmail.com>
2020-05-15 15:27:25 +0900

    チャット自動位更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,4 +62,36 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
 });
 })
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) { 
+   //追加するHTMLの入れ物を作る
+   var insertHTML = '';
+   //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+   $.each(messages, function(i, message) {
+     insertHTML += buildHTML(message)
+   });
+   //メッセージが入ったHTMLに、入れ物ごと追加
+   $('.messages').append(insertHTML);
+   $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
